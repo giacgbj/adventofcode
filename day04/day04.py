4bb40556d988eef31cabcf37f9ef9d5db0bba265
Giacomo Boccardo <gboccard@gmail.com>
2015-12-05 15:41:04 +0100

    Day 4 solutions

---
+++ day04/day04.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Advent of Code 2015
+#
+# Giacomo Boccardo 2015
+
+import hashlib
+
+secret_key = "yzbqklnj"
+
+def find_lowest_positive_number(zeros):
+    lowest_positive_number = -1
+    md5 = ""
+    while not md5.startswith("0" * zeros):
+        lowest_positive_number += 1
+        md5 = hashlib.md5((secret_key + str(lowest_positive_number)).encode('utf-8')).hexdigest()
+    return lowest_positive_number
+
+
+print("Part 1:", find_lowest_positive_number(5))
+
+print("Part 2:", find_lowest_positive_number(6))
